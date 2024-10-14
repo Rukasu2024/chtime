13231fbabdbcef742c1289e893f36bb000823286
Rukasu2024 <rukasutrader@gmail.com>
2024-10-14 21:02:14 +0200

    keep_alive.py aktualisieren

--- keep_alive.py
+++ keep_alive.py
@@ -1,16 +1,14 @@
 from flask import Flask
 from threading import Thread
-import os
 
 app = Flask('')
 
 @app.route('/')
 def home():
-    return "Hello. I am alive!"
+    return "I'm alive"
 
 def run():
-    port = int(os.getenv("PORT", 8080))  # Standard-Port auf 8080 setzen, falls keine Umgebungsvariable existiert
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=8080)
 
 def keep_alive():
     t = Thread(target=run)
